c00ca35cd8a780834722e1917f295acb56a8db03
JA <pride.ja0707@gmail.com>
2024-02-06 22:21:02 +0800

    11

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import styles from './index.less'
-import { Card, Toast, Button, JumboTabs, Divider, CapsuleTabs } from 'antd-mobile'
+import { Card, Toast, SpinLoading, JumboTabs, Divider, CapsuleTabs } from 'antd-mobile'
 import { AddCircleOutline, EditSFill } from 'antd-mobile-icons';
-import ReactEcharts from 'echarts-for-react';
-import { useNavigate } from 'react-router-dom';
+import { Chart, Axis, Geom, Tooltip, Guide } from 'bizgoblin';
+import { useNavigate, useLocation } from 'react-router-dom';
 import moment from 'moment'
 export default function () {
     const navigate = useNavigate();
-    const sugarRate = 0.3
+    const params: any = useLocation();
+    const sugarRate = 0.8
     const sugarTargetLow = 4.4
     const sugarTargetHigh = 7.8
     const girth = 2 * Math.PI * 50
     const dasharray = `${sugarRate * girth} ${girth}`
     const d = " M 100, 50 A 50, 50, 0, 0, 1, 100, 150 A 50, 50, 0, 0, 1, 100, 50"
-    const onClick = () => {
-        Toast.show('点击了卡片')
-    }
+    console.log(params, 'jap');
+
     const [showTime, setShowTime] = useState([])
     const [showData, setShowData] = useState([])
+    const [showArr, setShowArr] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
     const getData = async () => {
         try {
-            const tmp = await fetch("http://8.134.86.162:8080/predict/")
+            const obj: any = {}
+            params?.search?.split('?')[1]?.split('&')?.forEach((i: any) => {
+                const key = i.split('=')[0]
+                const val = i.split('=')[1]
+                obj[key] = key === 'newGlucose' ? val.split(',') : val
+            })
+            const { newGlucose = [] } = obj
+
+            const tmp = await fetch("http://8.134.86.162:8080/predict/predictAfterEat",
+                {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        newGlucose,
+                        weight: ''
+                    })
+                }
+            )
             const res = JSON.parse(await tmp.text())
             const { result } = res
             const showArr = result.slice(-20).map((i: any) => {
-                console.log(i.time, 'jai');
                 i.time = moment(i.time).format('HH:mm:ss')
-                return i
+                return {
+                    date: i.time,
+                    value: i.predictValue
+                }
             })
             const timeArr = showArr.map((i: any) => {
                 return i.time
@@ -35,8 +58,10 @@ export default function () {
                 return i.predictValue
             })
             console.log(showArr, 'jar');
+            setShowArr(showArr)
             setShowTime(timeArr)
             setShowData(dataArr)
+            setIsLoading(false)
         } catch (error) {
             console.log(error, 'jae');
         }
@@ -47,29 +72,30 @@ export default function () {
         getData()
     }, [])
 
-    // TODO:换chart库
-    const op = {
-        xAxis: {
-            type: 'category',
-            data: showTime
-        },
-        yAxis: {
-            type: 'value',
-            min: 84,
-            max: 86
-        },
-        series: [
-            {
-                data: showData,
-                type: 'line',
-            }
-        ]
-    }
-
     const dataInput = () => {
         navigate('/dataInput')
     }
+    const defs = [
+        {
+            dataKey: 'value',
+            tickCount: 5,
+            min: 0,
+            formatter: (val: any) => {
+                return Math.round(val * 100) / 100
+            }
+        },
+        {
+            dataKey: 'date',
+            tickCount: 5,
+        },
+    ];
 
+    const onShowTooltip = (i: any) => {
+        const { items } = i
+        items[0].title = `时间：${items[0].origin.date}`
+        items[0].name = '血糖值'
+        items[0].value = items[0].origin.value
+    }
     return (
         <div className={styles['wrapper']}>
             <div className={styles.top}>
@@ -83,28 +109,75 @@ export default function () {
                             <div className={styles['center']}>
                                 <p style={{ marginBottom: '3px' }}> {sugarRate * 100}%</p>
                                 <div onClick={dataInput}> <AddCircleOutline fontSize={28} /></div>
-                                {/* TODO:此处给出推荐值 */}
                             </div>
                         </div>
                         <div className={styles['target']}>{`控糖目标:${sugarTargetLow}~${sugarTargetHigh}mmol/L`}<EditSFill color='blue' fontSize={'16px'} /></div>
+                        <Divider></Divider>
+                        <CapsuleTabs>
+                            <CapsuleTabs.Tab title='血糖' key='fruits'>.
+                                <div style={{
+                                    position: 'relative',
+                                    height: '300px',
+                                    width: '90vw'
+                                }}>
+                                    {isLoading ?
+                                        <div className={styles['loading']}> <SpinLoading style={{ '--size': '48px' }} /> </div>
+                                        :
+                                        // TODO:拖拽
+                                        <Chart
+                                            width="100%"
+                                            height="100%"
+                                            data={showArr}
+                                            defs={defs}
+                                            padding={[45, 45, 50, 65]}
+                                        >
+                                            <Axis dataKey="date" />
+                                            <Axis dataKey="value" />
+                                            <Geom geom="path" position="date*value" />
+                                            <Tooltip onShow={onShowTooltip} showTitle={true} />
+                                            <Guide
+                                                type='text'
+                                                top={true}
+                                                position={['median', 'max']}
+                                                offsetY={240}
+                                                content={'时间'}
+                                                style={{
+                                                    fill: '#999',
+                                                    fontSize: 12
+                                                }}
+                                            >
+                                            </Guide>
+                                            <Guide
+                                                type='text'
+                                                top={true}
+                                                position={['min', 'max']}
+                                                offsetX={-50}
+                                                offsetY={90}
+                                                content={'血\n糖\n值'}
+                                                style={{
+                                                    fill: '#999',
+                                                    fontSize: 12
+                                                }}
+                                            >
+                                            </Guide>
+                                        </Chart>
+                                    }
+                                </div>
+                            </CapsuleTabs.Tab>
+                            <CapsuleTabs.Tab title='注射记录' key='vegetables'>
+                                注射记录
+                            </CapsuleTabs.Tab>
+                            <CapsuleTabs.Tab title='建议' key='animals'>
+                                建议
+                            </CapsuleTabs.Tab>
+                        </CapsuleTabs>
                     </JumboTabs.Tab>
                     <JumboTabs.Tab title='尿酸' description='' key='uric'>
                         尿酸
                     </JumboTabs.Tab>
                 </JumboTabs>
-                <Divider></Divider>
-                <CapsuleTabs>
-                    <CapsuleTabs.Tab title='血糖' key='fruits'>
-                        <ReactEcharts option={op} style={{ height: '300px' }} />
-                    </CapsuleTabs.Tab>
-                    <CapsuleTabs.Tab title='注射记录' key='vegetables'>
-                        <ReactEcharts option={op} style={{ height: '300px' }} />
-                    </CapsuleTabs.Tab>
-                    <CapsuleTabs.Tab title='建议' key='animals'>
-                        建议
-                    </CapsuleTabs.Tab>
-                </CapsuleTabs>
             </div >
         </div>
     )
 }
+
--- src/pages/InputData/index.tsx
+++ src/pages/InputData/index.tsx
@@ -6,60 +6,71 @@ import {
     Input,
     Button,
     NavBar,
-    TextArea,
-    DatePicker,
-    Selector,
-    Slider,
-    Stepper,
-    Switch,
+    CalendarPicker
 } from 'antd-mobile'
+import moment from 'moment'
 export default function () {
     const navigate = useNavigate();
+    // const form = useForm()
     const back = () => {
         navigate(-1)
     }
+    const finish = (data: any) => {
+        console.log(data, 'jad');
+        const { newGlucose, weight } = data
+        navigate(`/home?newGlucose=${newGlucose}&weight=${weight}`)
+    }
+    const [visible1, setVisible1] = useState(false)
+    const [time, setTime] = useState(null)
+    const singleDate: Date = new Date('2023-06-03')
+
     return (
         <div className={styles['wrapper']}>
-            <NavBar onBack={back}>数据输入</NavBar>
+            <NavBar onBack={back}>血糖值记录</NavBar>
             <Form
                 layout='horizontal'
+                onFinish={finish}
                 footer={
                     <Button block type='submit' color='primary' size='large'>
                         保存
                     </Button>
                 }
             >
-                <Form.Header>数据提交</Form.Header>
+                <Form.Header>血糖值记录</Form.Header>
                 <Form.Item
-                    name='suger'
+                    name='newGlucose'
                     label='血糖'
+                    help='血糖'
                     rules={[{ required: true, message: '血糖不能为空' }]}
                 >
-                    <Input onChange={console.log} placeholder='请输入血糖' />
+                    <Input type='number' min={50} max={150} placeholder='请输入血糖' />
                 </Form.Item>
-                <Form.Item name='address' label='时间' help='详细'>
-                    <Input
-                        placeholder='请输入时间'
+                <Form.Item
+                    name='newGlucose'
+                    label='时间'
+                    help='时间'
+                    rules={[{ required: true, message: '时间' }]}
+                >
+                    {/* {time ? moment(time).format('YYYY-MM-DD HH:mm:ss') : "选择单个日期"} */}
+                    <Input value={time ? moment(time).format('YYYY-MM-DD HH:mm:ss') : ""}></Input>
+                    <div onClick={() => {
+                        setVisible1(true)
+                    }}>选择日期</div>
+                    <CalendarPicker
+                        visible={visible1}
+                        selectionMode='single'
+                        defaultValue={singleDate}
+                        onClose={() => setVisible1(false)}
+                        onMaskClick={() => setVisible1(false)}
+                        onConfirm={(val: any) => {
+                            console.log(val, 'jav');
+                            // form.s
+                            setTime(val)
+                        }}
                     />
-                </Form.Item>
-                <Form.Item name='amount' label='用药' childElementPosition='right'>
-                    <Stepper />
-                </Form.Item>
-                <Form.Item
-                    name='delivery'
-                    label='饮食'
-                    childElementPosition='right'
-                >
-                    <Switch />
-                </Form.Item>
-                <Form.Item
-                    name='text'
-                    label='备注'
-                >
-                    <TextArea placeholder='请输入备注信息'/>
                 </Form.Item>
             </Form>
 
-        </div>
+        </div >
     )
 }
